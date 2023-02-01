a344b0b7611c5b0b36eaef34d60e5b8fba03e67d
LeQuangTung <lequangtung1410@gmail.com>
2023-02-01 09:29:49 +0700

    bài tập

---
+++ service_router/src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TimelinesComponent } from './timelines/timelines.component';
+import { PlaylistComponent } from './playlist/playlist.component';
+import { YoutubePlaylistComponent } from './youtube-playlist/youtube-playlist.component';
+import { YoutubePlayerComponent } from './youtube-player/youtube-player.component';
+import { ProductCreateComponent } from './product/product-create/product-create.component';
+import { ProductListComponent } from './product/product-list/product-list.component';
+import {ReactiveFormsModule} from "@angular/forms";
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TimelinesComponent,
+    PlaylistComponent,
+    YoutubePlaylistComponent,
+    YoutubePlayerComponent,
+    ProductCreateComponent,
+    ProductListComponent
+  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        ReactiveFormsModule
+    ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
